85dd457739ffcda19b9eabfdfa247065097925fd
edmondgaks <edmondgaks@gmail.com>
2023-01-24 12:21:26 +0200

    suppressing warnings

---
+++ Annotations/src/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    String name;
+    int age;
+    public  Cat(String name) { this.name = name; }
+    public void meow() {
+        System.out.println("Meow!!");
+    }
+    public void eat() {
+        System.out.println("Munch!!");
+    }
+}
---
+++ Annotations/src/CreatingAnnotations.java
@@ -0,0 +1,6 @@
+public class CreatingAnnotations {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        Cat myCat = new Cat("Stella");
+    }
+}
