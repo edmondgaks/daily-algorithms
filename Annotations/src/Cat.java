ddf2642b6852e5faf92cd90469343e8152afa8b2
edmondgaks <edmondgaks@gmail.com>
2023-01-24 20:35:28 +0200

    annotations

--- Annotations/src/Cat.java
+++ Annotations/src/Cat.java
@@ -1,11 +1,15 @@
+@VeryImportant
 public class Cat {
     String name;
     int age;
     public  Cat(String name) { this.name = name; }
+    @RunImmediately
     public void meow() {
         System.out.println("Meow!!");
     }
+
     public void eat() {
         System.out.println("Munch!!");
     }
+
 }
--- Annotations/src/CreatingAnnotations.java
+++ Annotations/src/CreatingAnnotations.java
@@ -1,6 +1,17 @@
+import java.lang.reflect.Method;
 public class CreatingAnnotations {
     public static void main(String[] args) {
         @SuppressWarnings("unused")
         Cat myCat = new Cat("Stella");
+        if(myCat.getClass().isAnnotationPresent(VeryImportant.class)) {
+            System.out.println("This thing is very important");
+        } else {
+            System.out.println("This thing is not important");
+        }
+        for(Method method : myCat.getClass().getDeclaredMethods()) {
+            if(method.isAnnotationPresent(RunImmediately.class)) {
+                method.invoke(myCat);
+            }
+        }
     }
 }
